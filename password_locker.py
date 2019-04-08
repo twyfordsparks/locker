9b6694508a5ddf941a41186aa0ca11aecddf5668
john victor <johnvictor0002@gmail.com>
2019-04-08 13:30:30 +0300

    added function that generates paswwords randomly

--- password_locker.py
+++ password_locker.py
@@ -22,3 +22,16 @@ def verify_user(first_name, password):
 	'''
 	checking_user = Credential.check_user(first_name, password)
 	return checking_user
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
+
+def create_credential(user_name,site_name,account_name,password):
+	'''
+	Function to create a new credential
+	'''
+	new_credential=Credential(user_name,site_name,account_name,password)
+	return new_credential
