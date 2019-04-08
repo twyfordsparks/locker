8ce631a863e06bb35a8359b5eec0f64dbf4a79bf
john victor <johnvictor0002@gmail.com>
2019-04-08 13:22:52 +0300

    added function that saves a new user

--- password_locker.py
+++ password_locker.py
@@ -8,3 +8,17 @@ def create_user(fname, lname, password):
 	'''
 	new_user = User(fname, lname, password)
 	return new_user
+
+def save_user(user):
+	'''
+	Function to save a new user account
+	'''
+	User.save_user(user)
+
+
+def verify_user(first_name, password):
+	'''
+	Function that verifies the existance of the user before creating credentials
+	'''
+	checking_user = Credential.check_user(first_name, password)
+	return checking_user
