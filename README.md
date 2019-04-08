79e11279b433d3c4e2825800415961bc2a771e01
john victor <johnvictor0002@gmail.com>
2019-04-08 14:20:53 +0300

    add readme content

---
+++ README.md
@@ -0,0 +1,43 @@
+# Password Locker
+### Password locker is a simple python application that stores and generates passwords for different user accounts
+### April,08, 2019
+#### By **[John Victor Njoroge Karanja](https://github.com/twyfordsparks)**
+
+## Description
+Password Locker is an application that helps users generate and store passwords for their multiple social or online platforms accounts.
+The password locker runs on the terminal and uses short codes to navigate through it.
+When starting up the application, the user is met with the following shortcodes:
+
+    1. cc - Creates a new credential
+    2. dc - Displays credentials
+    3. fc - Find a credential
+    4. ex - Exit the application
+
+## project's BDD
+| Behaviour | Input | Output |
+| :---------------- | :---------------: | ------------------: |
+| Display codes for navigation | **In terminal: $./password_locker.py** | Welcome, choose an option: ca-Create Account, li-Log In, ex-Exit |
+| Display prompt for creating an account | **Enter: ca** | Enter your first name, last name and password |
+| Display prompt for login in | **Enter: li** | Enter your account name and password |
+| Display codes for navigation | **Successful login** | Choose an option: cc - Create Credential, dc - Display Credentials, copy - Copy Credential, ex - exit |
+| Display prompt for creating a credential | **Enter: cc** | Enter the site name, your username and password |
+| Display a list of credentials | **Enter: dc** | Prints a list of saved credentials |
+| Display prompt for which credential to copy | **Enter: copy** | Enter the site name of the credential you wish to copy. |
+| Exit application | **Enter: ex** | Exit the current navigation stage |
+
+## Prerequisites
+    - Python 3.6 required
+
+## Set-up and Installation
+    - Clone the Repository
+    - Install python 3.6
+    - Run `python3.6 run.py`
+
+## Known bugs
+No known errors as at the completion of the project but in case of any bug or error,kindly contact me through the email,johnvictor0002@gmail.com .
+
+## Technologies used
+    - Python 3.6
+
+## Support and contact details
+Contact me at johnvictor0002@gmail.com for any comments, reviews or advice.
---
+++ user_credentials_test.py
@@ -0,0 +1,131 @@
+import unittest
+import pyperclip
+from user_credentials import User, Credential
+
+
+class TestUser(unittest.TestCase):
+	'''
+	Test class that defines test cases for the user class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+
+	def setUp(self):
+		'''
+		Function to create a user account before each test
+		'''
+		self.new_user = User('John', 'Victor', '2000')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of user instances is properly done
+		'''
+		self.assertEqual(self.new_user.first_name, 'John')
+		self.assertEqual(self.new_user.last_name, 'Victor')
+		self.assertEqual(self.new_user.password, '2000')
+
+	def test_save_user(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list), 1)
+
+
+class TestCredentials(unittest.TestCase):
+	'''
+	Test class that defines test cases for the credentials class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+
+	def test_check_user(self):
+		'''
+		Function to test whether the login in function check_user works as expected
+		'''
+		self.new_user = User('John', 'Victor', '2000')
+		self.new_user.save_user()
+		user2 = User('John', 'Victor', '2000')
+		user2.save_user()
+
+		for user in User.users_list:
+			if user.first_name == user2.first_name and user.password == user2.password:
+				current_user = user.first_name
+		return current_user
+
+		self.assertEqual(current_user, Credential.check_user(
+			user2.password, user2.first_name))
+
+	def setUp(self):
+		'''
+		Function to create an account's credentials before each test
+		'''
+		self.new_credential = Credential(
+			'twyford', 'Facebook', 'twyfordsparks', 'johnjane')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+		self.assertEqual(self.new_credential.user_name, 'Sparks')
+		self.assertEqual(self.new_credential.site_name, 'Facebook')
+		self.assertEqual(self.new_credential.account_name, 'twyfordsparks')
+		self.assertEqual(self.new_credential.password, 'johnjane')
+
+	def test_save_credentials(self):
+		'''
+		Test to check if the new credential info is saved into the credentials list
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('twyford', 'Twitter', 'twyfordsparks', 'johnvictor')
+		twitter.save_credentials()
+		self.assertEqual(len(Credential.credentials_list), 2)
+
+	def tearDown(self):
+		'''
+		Function to clear the credentials list after every test
+		'''
+		Credential.credentials_list = []
+		User.users_list = []
+
+	def test_display_credentials(self):
+		'''
+		Test to check if the display_credentials method, displays the correct credentials.
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('twyford', 'Twitter', 'johnvictor', '2000')
+		twitter.save_credentials()
+		gmail = Credential('twyford', 'Gmail', 'johnvictor', '2000')
+		gmail.save_credentials()
+		self.assertEqual(len(Credential.display_credentials(twitter.user_name)), 2)
+
+	def test_find_by_site_name(self):
+		'''
+		Test to check if the find_by_site_name method returns the correct credential
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('twyford', 'Twitter', 'johnvictor', '2000')
+		twitter.save_credentials()
+		credential_exists = Credential.find_by_site_name('Twitter')
+		self.assertEqual(credential_exists, twitter)
+
+	def test_copy_credential(self):
+		'''
+		Test to check if the copy a credential method copies the correct credential
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('twyford', 'Twitter', 'johnvictor', '2000')
+		twitter.save_credentials()
+		find_credential = None
+		for credential in Credential.user_credentials_list:
+				find_credential = Credential.find_by_site_name(credential.site_name)
+				return pyperclip.copy(find_credential.password)
+		Credential.copy_credential(self.new_credential.site_name)
+		self.assertEqual('2000', pyperclip.paste())
+		print(pyperclip.paste())
+
+
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
