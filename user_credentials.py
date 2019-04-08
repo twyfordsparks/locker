b9602b6dcaebaf85ebee1e83171e6220bf26c9e0
john victor <johnvictor0002@gmail.com>
2019-04-08 13:40:24 +0300

    added the class that checks if password entered are correct and saves newly added accounts

--- user_credentials.py
+++ user_credentials.py
@@ -29,3 +29,40 @@ class User:
 		Function to save a newly created user instance
 		'''
 		User.users_list.append(self)
+
+
+class Credential:
+	'''
+	Class to create  account credentials, generate passwords and save their information
+	'''
+	# Class Variables
+	credentials_list = []
+	user_credentials_list = []
+	@classmethod
+	def check_user(cls, first_name, password):
+		'''
+		Method that checks if the name and password entered match entries in the users_list
+		'''
+		current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
+
+	def __init__(self, user_name, site_name, account_name, password):
+		'''
+		Method to define the properties for each user object will hold._
+		'''
+
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
+
+	def save_credentials(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		# global users_list
+		Credential.credentials_list.append(self)
